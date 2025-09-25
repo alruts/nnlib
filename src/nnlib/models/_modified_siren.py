53bb23fff0f67520aa3e0acaf05f647ec5fbe603
Sturla Njardarson <sturla.njardarson@hbkworld.com>
2025-09-25 17:35:23 +0200

    chore: add typehinting

--- src/nnlib/models/_modified_siren.py
+++ src/nnlib/models/_modified_siren.py
@@ -20,11 +20,24 @@ class ModifiedSIREN(eqx.Module):
     - key: Random key.
     - in_features: Number of input features.
     - hidden_features: Number of hidden features.
-    - hidden_layers: Number of hidden layers.
+    - num_hidden: Number of hidden layers.
     - out_features: Number of output features.
     - outermost_linear: Whether the last layer is linear.
     - first_omega_0: Frequency of the first layer.
     - hidden_omega_0: Frequency of the hidden layers.
+
+    Example:
+        >>> import jax
+        >>> import jax.numpy as jnp
+        >>> import equinox as eqx
+        >>> key = jax.random.PRNGKey(0)
+        >>> siren = ModifiedSIREN(in_features=3, hidden_features=4, num_hidden=2, out_features=2, key=key)
+        >>> x, y, z = 0.1, 0.2, 0.3
+        >>> y = siren(x, y, z)
+        >>> isinstance(y, jax.Array)
+        True
+        >>> y.shape
+        (2,)
 
     [1] V. Sitzmann, J. N. P. Martel, A. W. Bergman, D. B. Lindell, and G.
     Wetzstein, "Implicit Neural Representations with Periodic Activation
@@ -37,46 +50,33 @@ class ModifiedSIREN(eqx.Module):
     Available: http://arxiv.org/abs/2001.04536
     """
 
-    layers: list
+    layers: tuple[SineLayer, ...]
     u: SineLayer
     v: SineLayer
-    in_features: int
-    hidden_features: int
-    hidden_layers: int
-    out_features: int
+    in_features: int = eqx.field(static=True)
+    out_features: int = eqx.field(static=True)
+    hidden_features: int = eqx.field(static=True)
+    num_hidden: int = eqx.field(static=True)
 
     def __init__(
         self,
-        in_features,
-        hidden_features,
-        hidden_layers,
-        out_features,
-        outermost_linear=False,
-        first_omega_0=30,
-        hidden_omega_0=30.0,
-        key=jrandom.PRNGKey(0),
+        in_features: int,
+        out_features: int,
+        hidden_features: int,
+        num_hidden: int,
+        first_omega_0: float = 30.0,
+        hidden_omega_0: float = 30.0,
+        outermost_linear: bool = False,
+        key: jax.Array = jrandom.PRNGKey(0),
     ):
         # Initialize the model
         self.in_features = in_features
-        self.hidden_features = hidden_features
-        self.hidden_layers = hidden_layers
         self.out_features = out_features
+        self.hidden_features = hidden_features
+        self.num_hidden = num_hidden
 
-        last_key, *keys = jax.random.split(key, hidden_layers + 4)
+        last_key, *keys = jax.random.split(key, num_hidden + 4)
         keys_iter = iter(keys)
-
-        self.layers = []
-
-        # First layer
-        self.layers.append(
-            SineLayer(
-                omega_0=first_omega_0,
-                is_first=True,
-                in_features=in_features,
-                out_features=hidden_features,
-                key=next(keys_iter),
-            )
-        )
 
         # u and v layers
         self.u = SineLayer(
@@ -94,17 +94,26 @@ class ModifiedSIREN(eqx.Module):
             key=next(keys_iter),
         )
 
+        # First layer
+        first_layer = SineLayer(
+            omega_0=first_omega_0,
+            is_first=True,
+            in_features=in_features,
+            out_features=hidden_features,
+            key=next(keys_iter),
+        )
+
         # Hidden layers
-        for _ in range(hidden_layers):
-            self.layers.append(
-                SineLayer(
-                    omega_0=hidden_omega_0,
-                    is_first=False,
-                    in_features=hidden_features,
-                    out_features=hidden_features,
-                    key=next(keys_iter),
-                )
+        hidden_layers = (
+            SineLayer(
+                omega_0=hidden_omega_0,
+                is_first=False,
+                in_features=hidden_features,
+                out_features=hidden_features,
+                key=next(keys_iter),
             )
+            for _ in range(num_hidden)
+        )
 
         # Last layer
         if outermost_linear:
@@ -130,7 +139,9 @@ class ModifiedSIREN(eqx.Module):
                 out_features=out_features,
                 key=last_key,
             )
-        self.layers.append(last_layer)
+
+        # Compile layers into tuple
+        self.layers = (first_layer, *hidden_layers, last_layer)
 
     def __call__(self, *args):
         """Forward pass."""
--- src/nnlib/models/_siren.py
+++ src/nnlib/models/_siren.py
@@ -18,11 +18,24 @@ class SIREN(eqx.Module):
         - key: Random key.
         - in_features: Number of input features.
         - hidden_features: Number of hidden features.
-        - hidden_layers: Number of hidden layers.
+        - num_hidden: Number of hidden layers.
         - out_features: Number of output features.
         - outermost_linear: Whether the last layer is linear.
         - first_omega_0: Frequency of the first layer.
         - hidden_omega_0: Frequency of the hidden layers.
+
+    Example:
+        >>> import jax
+        >>> import jax.numpy as jnp
+        >>> import equinox as eqx
+        >>> key = jax.random.PRNGKey(0)
+        >>> siren = SIREN(in_features=3, hidden_features=4, num_hidden=2, out_features=2, key=key)
+        >>> x, y, z = 0.1, 0.2, 0.3
+        >>> y = siren(x, y, z)
+        >>> isinstance(y, jax.Array)
+        True
+        >>> y.shape
+        (2,)
 
     [1] V. Sitzmann, J. N. P. Martel, A. W. Bergman, D. B. Lindell, and G.
     Wetzstein, "Implicit Neural Representations with Periodic Activation
@@ -30,61 +43,57 @@ class SIREN(eqx.Module):
     Available: http://arxiv.org/abs/2006.09661
     """
 
-    layers: list
-    in_features: int
-    hidden_features: int
-    hidden_layers: int
-    out_features: int
+    layers: tuple[SineLayer, ...]
+    in_features: int = eqx.field(static=True)
+    out_features: int = eqx.field(static=True)
+    hidden_features: int = eqx.field(static=True)
+    num_hidden: int = eqx.field(static=True)
 
     def __init__(
         self,
-        in_features,
-        hidden_features,
-        hidden_layers,
-        out_features,
-        outermost_linear=False,
-        first_omega_0=30,
-        hidden_omega_0=30.0,
-        key=jrandom.PRNGKey(0),
+        in_features: int,
+        hidden_features: int,
+        num_hidden: int,
+        out_features: int,
+        outermost_linear: bool = False,
+        first_omega_0: float = 30.0,
+        hidden_omega_0: float = 30.0,
+        key: jax.Array = jrandom.PRNGKey(0),
     ):
         # Initialize the model
         self.in_features = in_features
         self.hidden_features = hidden_features
-        self.hidden_layers = hidden_layers
+        self.num_hidden = num_hidden
         self.out_features = out_features
 
-        keys = jax.random.split(key, hidden_layers + 2)
-        first_key, *hidden_keys, last_key = keys
-
-        self.layers = []
+        last_key, *keys = jax.random.split(key, num_hidden + 2)
+        keys_iter = iter(keys)
 
         # First layer
-        self.layers.append(
-            SineLayer(
-                omega_0=first_omega_0,
-                is_first=True,
-                in_features=in_features,
-                out_features=hidden_features,
-                key=first_key,
-            )
+        first_layer = SineLayer(
+            omega_0=first_omega_0,
+            is_first=True,
+            in_features=in_features,
+            out_features=hidden_features,
+            key=next(keys_iter),
         )
 
         # Hidden layers
-        for i in range(hidden_layers):
-            self.layers.append(
-                SineLayer(
-                    omega_0=hidden_omega_0,
-                    is_first=False,
-                    in_features=hidden_features,
-                    out_features=hidden_features,
-                    key=hidden_keys[i],
-                )
+        hidden_layers = (
+            SineLayer(
+                omega_0=hidden_omega_0,
+                is_first=False,
+                in_features=hidden_features,
+                out_features=hidden_features,
+                key=next(keys_iter),
             )
+            for _ in range(num_hidden)
+        )
 
         # Last layer
         if outermost_linear:
             init_key, last_key = jax.random.split(last_key)
-            final_layer = eqx.nn.Linear(
+            last_layer = eqx.nn.Linear(
                 hidden_features, out_features, use_bias=True, key=last_key
             )
 
@@ -93,21 +102,21 @@ class SIREN(eqx.Module):
             new_weights = jrandom.uniform(
                 init_key, (out_features, hidden_features), minval=-lim, maxval=lim
             )
-            final_layer = eqx.tree_at(
-                lambda layer: layer.weight, final_layer, new_weights
+            last_layer = eqx.tree_at(
+                lambda layer: layer.weight, last_layer, new_weights
             )
-            self.layers.append(final_layer)
 
         else:
-            self.layers.append(
-                SineLayer(
-                    omega_0=hidden_omega_0,
-                    is_first=False,
-                    in_features=hidden_features,
-                    out_features=out_features,
-                    key=last_key,
-                )
+            last_layer = SineLayer(
+                omega_0=hidden_omega_0,
+                is_first=False,
+                in_features=hidden_features,
+                out_features=out_features,
+                key=last_key,
             )
+
+        # Compile layers into tuple
+        self.layers = (first_layer, *hidden_layers, last_layer)
 
     def __call__(self, *args):
         """Forward pass."""
