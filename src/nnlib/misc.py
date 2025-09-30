fa0d33b2f76013786fd155494b5f21d739c8cbf2
Sturla Njardarson <sturla.njardarson@hbkworld.com>
2025-09-30 17:30:43 +0200

    update lift function

--- src/nnlib/misc.py
+++ src/nnlib/misc.py
@@ -1,5 +1,6 @@
-from typing import Any, Callable, Tuple
+from functools import wraps
 
+import equinox as eqx
 import jax
 import jax.numpy as jnp
 
@@ -11,19 +12,43 @@ def default_floating_dtype():
         return jnp.float32
 
 
-def lift_tuple(
-    f: Callable[[jnp.ndarray], Any],
-) -> Callable[..., Tuple[Any, ...]]:
+def lift_to_args(fn):
     """
-    Lift a single-array function to take any number of scalar arguments
-    and return a tuple of results.
+    Lift a function that expects a single array input to accept *args as scalars.
 
-    Example:
-        >>> import jax.numpy as jnp
-        >>> def sum_sq(arr):
-        ...     return jnp.sum(arr ** 2)
-        >>> lifted = lift_tuple(sum_sq)
-        >>> lifted(1, 2, 3)
-        (Array(1, dtype=int32), Array(4, dtype=int32), Array(9, dtype=int32))
+    >>> import jax
+    >>> import jax.numpy as jnp
+    >>> def dummy_net(x):
+    ...     x, y, z = x
+    ...     return x**2 + y**2 + z
+    >>> lifted_net = lift_to_args(dummy_net)
+    >>> # Call with scalars
+    >>> print(lifted_net(1.0, 2.0, 3.0))
+    8.0
+    >>> print(jax.grad(lifted_net, argnums=0)(1.0, 2.0, 3.0))
+    2.0
+    >>> print(jax.grad(lifted_net, argnums=1)(1.0, 2.0, 3.0))
+    4.0
+    >>> print(jax.grad(lifted_net, argnums=2)(1.0, 2.0, 3.0))
+    1.0
     """
-    return lambda *args: tuple(f(jnp.array([arg])) for arg in args)
+
+    @wraps(fn)
+    def wrapper(*args, **kwargs):
+        x = jnp.stack(args)
+        return fn(x, **kwargs)
+
+    return wrapper
+
+
+def apply_model(self, params, *args):
+    """Trick to enable gradient with respect to weights."""
+    _, static = eqx.partition(self.model, eqx.is_inexact_array)
+    model = eqx.combine(params, static)
+    return model(*args[: model.in_features])
+
+
+def get_parameters(self):
+    """Returns the parameters of the model."""
+    params, _ = eqx.partition(self.model, eqx.is_inexact_array)
+    return params
