bdb78b5e47fbe8498c0b27502600d6187b224dbb
Sturla Njardarson <sturlanjardarson@gmail.com>
2025-10-02 17:56:16 +0200

    update embeddings

--- src/nnlib/embeddings.py
+++ src/nnlib/embeddings.py
@@ -5,20 +5,20 @@ import jax
 import jax.numpy as jnp
 
 
-class PeriodEmbs(eqx.Module):
+class PeriodicEmbedding(eqx.Module):
     """Periodic embeddings with per-axis trainable periods applied to all inputs.
 
     Example:
         >>> import jax.numpy as jnp
         >>> import equinox as eqx
-        >>> periods = (jnp.array(1.0), jnp.array(2.0))
-        >>> emb = PeriodEmbs(periods=periods)
+        >>> periods = (1.0, 2.0)
+        >>> emb = PeriodicEmbedding(periods=periods)
         >>> x = jnp.array([0.0])
         >>> emb(x)
         Array([1., 0.], dtype=float32)
     """
 
-    periods: Tuple[jnp.ndarray, ...]
+    periods: Tuple[float, ...]
 
     def __call__(self, x):
         # Apply cos/sin embedding to each axis
@@ -30,7 +30,7 @@ class PeriodEmbs(eqx.Module):
         )
 
 
-class FourierEmbedding(eqx.Module):
+class RandomFourierEmbedding(eqx.Module):
     """Fourier embeddings with random Gaussian kernel
 
     Example:
@@ -38,7 +38,7 @@ class FourierEmbedding(eqx.Module):
         >>> import jax.numpy as jnp
         >>> import equinox as eqx
         >>> key = jax.random.PRNGKey(0)
-        >>> emb = FourierEmbedding(embed_scale=1.0, embed_dim=8, in_dim=3, key=key)
+        >>> emb = RandomFourierEmbedding(embed_scale=1.0, embed_dim=8, in_dim=3, key=key)
         >>> x = jnp.array([1.0, 2.0, 3.0])
         >>> y = emb(x)
         >>> y.shape
@@ -51,6 +51,9 @@ class FourierEmbedding(eqx.Module):
 
     def __init__(self, embed_scale, embed_dim, in_dim, *, key):
         _, key = jax.random.split(key, 2)
+
+        assert embed_dim % 2 == 0, "Embedded dimension must be a positive multiple of 2"
+
         self.kernel = jax.random.normal(key, (in_dim, embed_dim // 2)) * embed_scale
         self.embed_scale = embed_scale
         self.embed_dim = embed_dim
