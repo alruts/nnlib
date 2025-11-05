fdb5242db770971112bfd85f66b5a6d6bbf49ab0
Sturla Njardarson <sturlanjardarson@gmail.com>
2025-11-05 16:42:57 +0100

    enable direct import of default field params

--- src/nnlib/__init__.py
+++ src/nnlib/__init__.py
@@ -1,2 +1,6 @@
-def hello() -> str:
-    return "Hello from nnlib!"
+from nnlib.misc import default_medium_density, default_wave_speed
+
+__all__ = [
+    "default_wave_speed",
+    "default_medium_density",
+]
