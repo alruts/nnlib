9bed305c855fc881b415186eaabb213022ccc953
Sturla Njardarson <sturlanjardarson@gmail.com>
2025-11-05 20:49:20 +0100

    update rayleigh disk file and move into dedicated dir

--- src/nnlib/simulate_data/rayleigh_disk.py
+++ src/nnlib/simulate_data/rayleigh_disk.py
@@ -3,7 +3,8 @@ import jax
 import jax.numpy as jnp
 import matplotlib
 import matplotlib.pyplot as plt
-from jaxtyping import Array
+from jaxtyping import Array, Complex
+from matplotlib import patches
 
 from nnlib import default_medium_density, default_wave_speed
 from nnlib.data_utils import GridDiscretisationND
@@ -11,7 +12,9 @@ from nnlib.data_utils import GridDiscretisationND
 jax.config.update("jax_enable_x64", True)
 
 
-def plot_discretization(data: GridDiscretisationND, cbar_label="Value", **kwds):
+def plot_discretization(
+    data: GridDiscretisationND, cbar_label="Value", clim=None, **kwds
+):
     """
     Plots GridDiscretisationND object in 1D, 2D, or 3D using scatter with colorbar.
 
@@ -44,6 +47,9 @@ def plot_discretization(data: GridDiscretisationND, cbar_label="Value", **kwds):
         ax.set_zlabel("Z")
     else:
         raise ValueError("Only 1D, 2D, or 3D data supported.")
+
+    if clim:
+        sc.set_clim(clim)
 
     cbar = plt.colorbar(sc, ax=ax)
     cbar.set_label(cbar_label)
@@ -124,7 +130,7 @@ class RayleighDiskInBaffle(eqx.Module):
         self.disk_velocity = piston_velocity
 
     @eqx.filter_jit
-    def __call__(self, observation_point: jnp.ndarray) -> complex:
+    def __call__(self, observation_point: jnp.ndarray) -> Complex:
         """Compute complex acoustic pressure at a single observation point."""
         angular_frequency = 2 * jnp.pi * self.frequency
         wavenumber = angular_frequency / self.wave_speed
@@ -150,15 +156,16 @@ class RayleighDiskInBaffle(eqx.Module):
         return pressure
 
 
-# -------------------------
-# Example usage
-# -------------------------
 if __name__ == "__main__":
+    #
+    # # Define Rayleigh integral problem
+    #
+
     disk_model = RayleighDiskInBaffle(
         medium_density=default_medium_density(),
         wave_speed=default_wave_speed(),
         frequency=4000.0,
-        disk_radius=0.1,
+        disk_radius=0.0875,
         surface_impedance=2.0,
         piston_velocity=1.0,
         points_per_wavelength=7,
@@ -189,6 +196,32 @@ if __name__ == "__main__":
         n_points=[n_points_x, n_points_y, n_points_z],
     )
 
+    #
+    # # Plot magnitude and phase
+    #
+    for args in (["Magnitude", jnp.abs], ["Phase", jnp.angle]):
+        label, tx = args
+        fig, ax = plot_discretization(
+            domain.transform(tx), cbar_label=label, alpha=0.1, cmap="seismic"
+        )
+
+        # Plot the disk border
+        theta = jnp.linspace(0, 2 * jnp.pi, 200)  # parametric angle for circle
+        x_circle = disk_model.disk_radius * jnp.cos(theta)
+        y_circle = disk_model.disk_radius * jnp.sin(theta)
+        ax.plot(
+            x_circle, y_circle, "k--", linewidth=2, label="Piston Border", alpha=0.4
+        )
+
+        ax.legend()
+
+        plt.tight_layout()
+        plt.show()
+
+    #
+    # # Compute Helmholtz residual in whole domain to ensure clean signal for PINN
+    #
+
     def residual_fn(primal, model):
         def second_derivative_jvp(p, argnum=0):
             n = jnp.zeros_like(p)
@@ -217,13 +250,3 @@ if __name__ == "__main__":
 
     print(jnp.mean(jnp.abs(residual.vals)))
     print(jnp.max(jnp.abs(residual.vals)))
-
-    fig, ax = plot_discretization(
-        domain.transform(jnp.abs), cbar_label="Magnitude", alpha=0.1, cmap="seismic"
-    )
-    plt.show()
-
-    fig, ax = plot_discretization(
-        domain.transform(jnp.angle), cbar_label="Magnitude", alpha=0.1, cmap="seismic"
-    )
-    plt.show()
