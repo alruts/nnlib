0434750629ed45030f2d418954b0487848e15111
Sturla Njardarson <sturlanjardarson@gmail.com>
2025-11-16 20:21:29 +0100

    update disk demo

--- example_scripts/train_helmholtz_disk.py
+++ example_scripts/train_helmholtz_disk.py
@@ -1,14 +1,20 @@
+from collections.abc import Callable
+
 import equinox as eqx
 import jax
 import optax
+import trimesh
+from jax import Array
 from jax import numpy as jnp
 from jax import random as jrandom
+from jaxtyping import PyTree
 from matplotlib import pyplot as plt
 from soap_jax import soap
 from tqdm import tqdm
 
 from pinnlib import feature_maps
 from pinnlib.activations import (
+    LearnableSplitTanh,
     SplitSinActivation,
 )
 from pinnlib.data_utils import (
@@ -17,6 +23,7 @@ from pinnlib.data_utils import (
     subsample,
 )
 from pinnlib.data_utils.data_structures import GridDiscretisationND
+from pinnlib.data_utils.generators import MeshGenerator
 from pinnlib.logger import TensorboardLogger
 from pinnlib.losses import (
     aggregated_metrics,
@@ -41,18 +48,13 @@ from pinnlib.simulate_data.rayleigh_disk import RayleighDiskInBaffle
 seed_key = jrandom.PRNGKey(0)
 data_key, subsample_key, net_key, emb_key, dom_key = jrandom.split(seed_key, 5)
 
-#
-# Construct infinite data generators for PDE collocation points
-# and to load random batches of data points
-#
-
-
+# Simulate a rigid piston using the Rayleigh integral method
 disk_in_baffle = RayleighDiskInBaffle(
     medium_density=default_medium_density(),
     wave_speed=default_wave_speed(),
     frequency=1000.0,
     disk_radius=0.1,
-    surface_impedance=2,
+    surface_impedance=0.2,
     piston_velocity=1.0,
     points_per_wavelength=10,
 )
@@ -71,11 +73,14 @@ n_points_x = int(2 * grid_extent / dx_obs)
 n_points_y = int(2 * grid_extent / dx_obs)
 n_points_z = int((2 * wavelength) / dx_obs)
 
+# domain lower bound
+lower_bound = 1e-6
+
 data = GridDiscretisationND.discretise_fn(
     bounds=[
         (-grid_extent, grid_extent),
         (-grid_extent, grid_extent),
-        (0.0001, wavelength),
+        (lower_bound, wavelength),
     ],
     fn=disk_in_baffle,
     n_points=[n_points_x, n_points_y, n_points_z],
@@ -83,35 +88,58 @@ data = GridDiscretisationND.discretise_fn(
 
 eval_data = data.slice(z=0)
 
+data_generator = DataPointGenerator(
+    point_cloud=subsample.random_sample(
+        data,
+        num_points=128,
+        key=subsample_key,
+    ),  # returns `PointCloud` with random samples
+    batch_size=128,
+    key=data_key,
+)
 
-datasets = [
-    DataPointGenerator(
-        point_cloud=subsample.random_sample(
-            data,
-            num_points=n,
-            key=subsample_key,
-        ),  # returns `PointCloud` with random samples
-        batch_size=128,
-        key=data_key,
-    )
-    for n in {128}
-]
-
-domain_sampler = UniformGenerator(
+domain_generator = UniformGenerator(
     [
         (-grid_extent, grid_extent),
         (-grid_extent, grid_extent),
-        (0.0, wavelength),
+        (lower_bound, wavelength),
     ],
     batch_size=256,
     key=dom_key,
 )
 
+mesh = trimesh.load_mesh("data/disk_01.stl")
+disk_generator = MeshGenerator(mesh, batch_size=128)
+
 # Here we define which loss functions to use during training
 # The losses should be parallelized and vectorized via `pmap` and `vmap`
-#
 
-losses = {"data": data_loss, "pde": hom_pde_loss}
+
+def boundary_velocity_loss(
+    fwd_params: PyTree,
+    fwd_model: HelmholtzPINN,
+    coords_normals: tuple[Array, Array],
+    criterion: Callable,
+    inv_params: PyTree,
+    inv_model: Callable,
+):
+    pts, tangents = coords_normals
+    n_dim = len(pts)
+
+    # vectorize and parallelize
+    batched_fwd_model = jax.vmap(fwd_model.v_net, in_axes=(None, *[0] * n_dim * 2))
+    parallel_fwd_model = jax.pmap(batched_fwd_model, in_axes=(None, *[0] * n_dim * 2))
+
+    batched_inv_model = jax.vmap(inv_model, in_axes=(None, *[0] * n_dim * 2))
+    parallel_inv_model = jax.pmap(batched_inv_model, in_axes=(None, *[0] * n_dim * 2))
+
+    fwd_pred = parallel_fwd_model(fwd_params, *pts, *tangents)
+    inv_pred = parallel_inv_model(inv_params, *pts, *tangents)
+
+    return criterion(fwd_pred, inv_pred)
+
+
+losses = {"data": data_loss, "pde": hom_pde_loss, "bnd": boundary_velocity_loss}
 update_weights_every = 1000
 
 # Initialize the weights for adaptive grad norm, these are updated after the first step
@@ -119,7 +147,7 @@ update_weights_every = 1000
 loss_weights = {key: jnp.array(1.0) for key in losses.keys()}
 
 
-# Helper to make predictions for logging
+# Helpers for logging
 @eqx.filter_jit
 def compute_pressure(params, pinn):
     """Evaluates pressure over the same grid as original dataset"""
@@ -127,140 +155,204 @@ def compute_pressure(params, pinn):
     return p(params, *eval_data.coordinate_arrays, 0.0)
 
 
-# Helper to make plots for logging
+@eqx.filter_jit
+def compute_velocity(params, pinn):
+    """Evaluates pressure over the same grid as original dataset"""
+    v = grid_map(pinn.v_net, axis_mask=(0, 1, 1, 0, 0, 0, 0))
+    return v(params, *eval_data.coordinate_arrays, 0.0, 0.0, 0.0, 1.0)
+
+
 def plot_pred(pressure):
     from matplotlib import colors
 
-    divnorm = colors.TwoSlopeNorm(vmin=-3.14, vcenter=0.0, vmax=3.14)
-
     fig, ax = plt.subplots()
     pc = ax.pcolormesh(
-        *eval_data.coordinate_arrays, pressure, cmap="seismic", norm=divnorm
+        *eval_data.coordinate_arrays,
+        pressure,
+        cmap="plasma",
     )
     ax.set_xlabel("X")
     ax.set_ylabel("Y")
 
     plt.colorbar(pc, ax=ax)
 
+    # Draw a dashed circle using plt.plot
+    theta = jnp.linspace(0, 2 * jnp.pi, 200)
+    x = disk_in_baffle.disk_radius * jnp.cos(theta)
+    y = disk_in_baffle.disk_radius * jnp.sin(theta)
+    ax.plot(x, y, "w--", linewidth=2)  # 'w--' = white dashed line
+
     ax.set_aspect("equal", "box")
     return fig
 
 
-#
-# Train the each setup in a loop
-#
+# These are infinitely iterable
+infinite_data_loader = iter(data_generator)
+infinite_point_loader = iter(domain_generator)
+infinite_disk_loader = iter(disk_generator)
 
-for dataset in datasets:
-    # These are infinitely iterable
-    infinite_dataloader = iter(dataset)
-    infinite_point_generator = iter(domain_sampler)
+# initialize logger
+logger = TensorboardLogger(experiment_name=f"test-run_{len(data_generator)}")
+logger.log_plot("plots/p_gt_mag", plot_pred, jnp.abs(eval_data.vals), 0)
+logger.log_plot("plots/p_gt_phase", plot_pred, jnp.angle(eval_data.vals), 0)
+log_every = 1000
 
-    # initialize logger
-    logger = TensorboardLogger(experiment_name=f"test-run_{len(dataset)}")
-    logger.log_plot(
-        "plots/gt_mag", plot_pred, jnp.abs(eval_data.vals), 0
-    )  # to visually compare
-    logger.log_plot("plots/gt_phase", plot_pred, jnp.angle(eval_data.vals), 0)
-    log_every = 1000
+# Random Fourier features for input coordinates helps with low-frequency bias
+rff = feature_maps.RandomFourierFeatures(
+    embed_scale=1 / jnp.sqrt(wavelength), embed_dim=128, in_dim=3, key=emb_key
+)
 
-    # Random Fourier features for input coordinates helps with low-frequency bias
-    rff = feature_maps.RandomFourierFeatures(
-        embed_scale=1 / jnp.sqrt(wavelength), embed_dim=128, in_dim=3, key=emb_key
+# build PINN
+pinn = HelmholtzPINN.create(
+    embedding=rff,
+    arch_name="siren",
+    frequency=disk_in_baffle.frequency,
+    in_size=3,
+    out_size="scalar",
+    width_size=16,
+    depth=3,
+    dtype=default_complex_dtype(),
+    first_activation=SplitSinActivation(float(wavenumber)),
+    activation=SplitSinActivation(30.0),
+    final_activation=LearnableSplitTanh(jnp.array(1.0), jnp.array(1.0)),
+    key=net_key,
+)
+
+# simply use ground truth
+velocity_model = lambda params, *_: params
+velocity_params = jnp.array(disk_in_baffle.disk_velocity)
+
+# Extract the trainable parameters of the neural-net as a `PyTree`
+pinn_params, _ = eqx.partition(pinn.model, filter_spec=eqx.is_array)
+pinn_params = jax.tree.map(
+    lambda x: x / jnp.sqrt(2), pinn_params
+)  # scaling due to complex
+
+# Initialize the Adam optimizer with learning rate scheduler
+learning_rate = optax.schedules.exponential_decay(1e-3, 2000, 0.9)
+fwd_optimizer = optax.contrib.split_real_and_imaginary(
+    soap(learning_rate, precondition_frequency=2)
+)
+
+inv_optimizer = optax.contrib.split_real_and_imaginary(optax.adam(learning_rate))
+fwd_opt_state = fwd_optimizer.init(pinn_params)
+inv_opt_state = inv_optimizer.init(velocity_params)
+
+
+# Define a single training step, `filter_jit` compiles this code to
+# machine-code
+
+
+@eqx.filter_jit
+def fwd_train_step(model, params, inv_model, inv_params, opt_state, weights, batch):
+    # extra arguments for boundary loss
+    extra_args = {"bnd": (inv_params, inv_model)}
+    (total, each_term), grads = jax.value_and_grad(compute_weighted_loss, has_aux=True)(
+        params,
+        model=model,
+        batch=batch,
+        weights=weights,
+        losses=losses,
+        criterion=split_real_and_imaginary_loss(aggregated_metrics["mse"]),
+        extra_args=extra_args,
+    )
+    grads_conj = jax.tree.map(jnp.conj, grads)
+    updates, opt_state = fwd_optimizer.update(grads_conj, opt_state, params)
+    params = optax.apply_updates(params, updates)
+
+    return params, opt_state, (total, each_term)
+
+
+@eqx.filter_jit
+def inv_train_step(model, params, inv_model, inv_params, opt_state, batch):
+    # extra arguments for boundary loss
+    val, grads = jax.value_and_grad(boundary_velocity_loss, argnums=4)(
+        params,
+        model,
+        batch["bnd"],
+        split_real_and_imaginary_loss(aggregated_metrics["mse"]),
+        inv_params,
+        inv_model,
+    )
+    grads_conj = jax.tree.map(jnp.conj, grads)
+    updates, opt_state = inv_optimizer.update(grads_conj, opt_state, inv_params)
+    inv_params = optax.apply_updates(inv_params, updates)
+
+    return inv_params, opt_state, val
+
+
+# Training loop: all the optimization work is done here + logging
+total_steps = int(5e3) + 1
+for step, data_batch, pde_batch, bnd_batch in tqdm(
+    zip(
+        range(total_steps),
+        infinite_data_loader,
+        infinite_point_loader,
+        infinite_disk_loader,
+    ),
+    total=total_steps,
+):
+    # The batch dictionary should have the same structure as `losses`
+    batch = {"data": data_batch, "pde": pde_batch, "bnd": bnd_batch}
+
+    # Do step
+    pinn_params, fwd_opt_state, (loss, individual_losses) = fwd_train_step(
+        pinn,
+        pinn_params,
+        velocity_model,
+        velocity_params,
+        fwd_opt_state,
+        loss_weights,
+        batch,
     )
 
-    # build PINN
-    pinn = HelmholtzPINN.create(
-        embedding=rff,
-        arch_name="siren",
-        frequency=disk_in_baffle.frequency,
-        in_size=3,
-        out_size="scalar",
-        width_size=16,
-        depth=3,
-        dtype=default_complex_dtype(),
-        first_activation=SplitSinActivation(float(wavenumber)),
-        activation=SplitSinActivation(30.0),
-        # final_activation=LearnableSplitTanh(jnp.array(1.0), jnp.array(1.0)),
-        key=net_key,
+    # adaptive inverse model
+    velocity_params, inv_opt_state, inv_loss = inv_train_step(
+        pinn,
+        pinn_params,
+        velocity_model,
+        velocity_params,
+        inv_opt_state,
+        batch,
     )
 
-    # Extract the trainable parameters of the neural-net as a `PyTree`
-    params, _ = eqx.partition(pinn.model, filter_spec=eqx.is_array)
-    params = jax.tree.map(lambda x: x / jnp.sqrt(2), params)  # scaling due to complex
-
-    # Initialize the Adam optimizer with learning rate scheduler
-    learning_rate = optax.schedules.exponential_decay(1e-3, 2000, 0.9)
-    optimizer = optax.contrib.split_real_and_imaginary(
-        soap(learning_rate, precondition_frequency=2)
-    )
-    opt_state = optimizer.init(params)  # Running state of the optimizer
-
-    #
-    # Define a single training step, `filter_jit` compiles this code to
-    # machine-code
-    #
-
-    @eqx.filter_jit
-    def train_step(model, params, opt_state, weights, batch):
-        (total, each_term), grads = jax.value_and_grad(
-            compute_weighted_loss, has_aux=True
-        )(
-            params,
-            model=model,
-            batch=batch,
-            weights=weights,
-            losses=losses,
-            criterion=split_real_and_imaginary_loss(aggregated_metrics["mse"]),
+    # Update adaptive weights
+    extra_args = {"bnd": (velocity_params, velocity_model)}
+    if step % update_weights_every == 0:
+        new_weights = compute_weights(
+            pinn_params, pinn, batch, losses, extra_args=extra_args
         )
-        grads_conj = jax.tree.map(jnp.conj, grads)
-        updates, opt_state = optimizer.update(grads_conj, opt_state, params)
-        params = optax.apply_updates(params, updates)
-        return params, opt_state, (total, each_term)
+        loss_weights = update_weights(0.9, loss_weights, new_weights)
 
-    #
-    # Training loop: all the optimization work is done here + logging
-    #
-
-    total_steps = int(5e3) + 1
-    for step, data_batch, pde_batch in tqdm(
-        zip(range(total_steps), infinite_dataloader, infinite_point_generator),
-        total=total_steps,
-    ):
-        # The batch dictionary should have the same structure as `losses`
-        batch = {"data": data_batch, "pde": pde_batch}
-
-        # Do step
-        params, opt_state, (loss, individual_losses) = train_step(
-            pinn, params, opt_state, loss_weights, batch
+    if step % log_every == 0:
+        logger.log_scalar("loss/total", loss, step)
+        logger.log_scalar("inv/loss", inv_loss, step)
+        logger.log_scalar(
+            "inv/velocity-error", velocity_params - disk_in_baffle.disk_velocity, step
         )
 
-        # Update adaptive weights
-        if step % update_weights_every == 0:
-            new_weights = compute_weights(params, pinn, batch, losses)
-            loss_weights = update_weights(0.9, loss_weights, new_weights)
+        for term, weight in loss_weights.items():
+            logger.log_scalar(f"weight/{term}", weight, step)
 
-        if step % log_every == 0:
-            logger.log_scalar("loss/total", loss, step)
+        for term, loss in individual_losses.items():
+            logger.log_scalar(f"loss/{term}", loss / loss_weights.get(term), step)
 
-            for term, weight in loss_weights.items():
-                logger.log_scalar(f"weight/{term}", weight, step)
+        # make a prediction
+        p_pred = compute_pressure(pinn_params, pinn)
+        v_pred = compute_velocity(pinn_params, pinn)
+        p_pred_error = split_real_and_imaginary_metric(aggregated_metrics["mse"])(
+            p_pred, eval_data.vals
+        )
 
-            for term, loss in individual_losses.items():
-                logger.log_scalar(f"loss/{term}", loss / loss_weights.get(term), step)
+        logger.log_scalar("errors/mse_re", p_pred_error.real, step)
+        logger.log_scalar("errors/mse_im", p_pred_error.imag, step)
 
-            # make a prediction
-            pred = compute_pressure(params, pinn)
+        logger.log_plot("plots/p_pred_mag", plot_pred, jnp.abs(p_pred), step)
+        logger.log_plot("plots/p_pred_phase", plot_pred, jnp.angle(p_pred), step)
+        logger.log_plot("plots/v_pred_mag", plot_pred, jnp.abs(v_pred), step)
+        logger.log_plot("plots/v_pred_phase", plot_pred, jnp.angle(v_pred), step)
 
-            pred_error = split_real_and_imaginary_metric(aggregated_metrics["mse"])(
-                pred, eval_data.vals
-            )
-
-            logger.log_scalar("errors/mse_re", pred_error.real, step)
-            logger.log_scalar("errors/mse_im", pred_error.imag, step)
-            logger.log_plot("plots/pred_mag", plot_pred, jnp.abs(pred), step)
-            logger.log_plot("plots/pred_phase", plot_pred, jnp.angle(pred), step)
-
-            # compute point-wise metrics
-            error = split_real_and_imaginary_metric(sq_error)(pred, eval_data.vals)
-            logger.log_plot("errors/sq_error_re", plot_pred, jnp.real(error), step)
-            logger.log_plot("errors/sq_error_im", plot_pred, jnp.imag(error), step)
+        # compute point-wise metrics
+        error = split_real_and_imaginary_metric(sq_error)(p_pred, eval_data.vals)
+        logger.log_plot("errors/sq_error_re", plot_pred, jnp.real(error), step)
+        logger.log_plot("errors/sq_error_im", plot_pred, jnp.imag(error), step)
