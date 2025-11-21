b17b18c3650fa4bf5129b33b74bd07fcdb56c601
Sturla Njardarson <sturla.njardarson@hbkworld.com>
2025-11-21 15:44:30 +0100

    update data and example scripts

--- example_scripts/train_helmholtz_piston_comsol.py
+++ example_scripts/train_helmholtz_piston_comsol.py
@@ -22,12 +22,14 @@ from pinnlib.activations import (
 from pinnlib.data_utils import (
     DataPointGenerator,
     MeshGenerator,
+    PointCloud,
     UniformGenerator,
+    pc_utils,
 )
-from pinnlib.data_utils.point_cloud import PointCloud, filter_points, sample_points
 from pinnlib.logger import TensorboardLogger
 from pinnlib.losses import (
     aggregated_metrics,
+    compute_mask,
     compute_weighted_loss,
     compute_weights,
     data_loss,
@@ -38,7 +40,6 @@ from pinnlib.metrics import sq_error
 from pinnlib.misc import (
     default_complex_dtype,
     default_wave_speed,
-    grid_map,
     split_real_and_imaginary_loss,
     split_real_and_imaginary_metric,
 )
@@ -56,8 +57,8 @@ with open(data_path, "rb") as f:
 # ...
 frequency = meta_data["frequency"]
 piston_radius = meta_data["piston_radius"]
-piston_velocity = meta_data["piston_velocity"]
-wave_speed = meta_data["wave_speed"]
+piston_velocity = 0.02
+wave_speed = default_wave_speed()
 
 # Derived acoustic quantities
 angular_frequency = 2 * jnp.pi * frequency
@@ -66,10 +67,18 @@ wavelength = 2 * jnp.pi / wavenumber
 
 # Observation grid
 grid_extent = 0.5 * wavelength + piston_radius
-lower_bound = 1e-5  # lower bound for hom pde loss
+lower_bound = 1e-9  # lower bound for hom pde loss
+
+# data pipeline
+data_pipe = pc_utils.pipe(
+    pc_utils.filter_points(lambda c, _: c[-1] <= 0.5 * wavelength),
+    pc_utils.filter_points(lambda c, _: c[-1] > 1e-2),
+    pc_utils.sample_points(subsample_key, 128),
+)
+data_pc = data_pipe(pressure_pc)
 
 data_generator = DataPointGenerator(
-    point_cloud=sample_points(subsample_key, 128)(pressure_pc),
+    point_cloud=data_pc,
     batch_size=128,
     key=data_key,
 )
@@ -84,14 +93,14 @@ domain_generator = UniformGenerator(
     key=dom_key,
 )
 
-mesh = trimesh.load_mesh("...")
-mesh_generator = MeshGenerator(mesh, batch_size=128, key=bnd_key)
+mesh = trimesh.load_mesh("./data/baffle.stl")
+mesh_generator = MeshGenerator(mesh, batch_size=32, key=bnd_key)
 
 
 # Filter evaluation data on surface plane
-z_filter = filter_points(lambda c, _: c[-1] < 1e-12)
-eval_pressure_pc = z_filter(pressure_pc)
-eval_velocity_pc = z_filter(velocity_pc)
+zero_z_filter = pc_utils.filter_points(lambda coord, _: coord[2] < 1e-12)
+eval_pressure_pc = zero_z_filter(pressure_pc)
+eval_velocity_pc = zero_z_filter(velocity_pc)
 
 # Here we define which loss functions to use during training
 # The losses should be parallelized and vectorized via `pmap` and `vmap`
@@ -109,7 +118,7 @@ def boundary_velocity_loss(
     n_dim = len(pts)
 
     # vectorize and parallelize
-    batched_fwd_model = jax.vmap(fwd_model.v_net, in_axes=(None, *[0] * n_dim * 2))
+    batched_fwd_model = jax.vmap(fwd_model.velocity, in_axes=(None, *[0] * n_dim * 2))
     parallel_fwd_model = jax.pmap(batched_fwd_model, in_axes=(None, *[0] * n_dim * 2))
 
     batched_inv_model = jax.vmap(inv_model, in_axes=(None, *[0] * n_dim * 2))
@@ -131,26 +140,25 @@ loss_weights = {key: jnp.array(1.0) for key in losses.keys()}
 
 # Helpers for logging
 @eqx.filter_jit
-def compute_pressure(params, pinn):
+def predict_pressure(params, pinn):
     """Evaluates pressure over the same grid as original dataset"""
-    p = grid_map(pinn.p_net, axis_mask=(0, 1, 1, 0))
-    return p(params, *eval_pressure_pc.coords.T, 0.0)
+    p_fn = jax.vmap(pinn, [None, 0, 0, None])
+    x, y, _ = eval_pressure_pc.coords
+    return p_fn(params, x, y, 0.0)
 
 
 @eqx.filter_jit
-def compute_velocity(params, pinn):
+def predict_velocity(params, pinn):
     """Evaluates pressure over the same grid as original dataset"""
-    v = grid_map(pinn.v_net, axis_mask=(0, 1, 1, 0, 0, 0, 0))
-    return v(params, *eval_pressure_pc.coords.T, 0.0, 0.0, 0.0, 1.0)
+    v = jax.vmap(pinn.velocity, [None, 0, 0, None, None, None, None])
+    x, y, _ = eval_pressure_pc.coords
+    return v(params, x, y, 0.0, 0.0, 0.0, 1.0)
 
 
 def plot_pred(pressure):
     fig, ax = plt.subplots()
-    pc = ax.scatter(
-        *eval_pressure_pc.coords.T,
-        c=pressure,
-        cmap="plasma",
-    )
+    x, y, _ = eval_pressure_pc.coords
+    pc = ax.scatter(x, y, c=pressure, cmap="seismic", s=5)
     ax.set_xlabel("X")
     ax.set_ylabel("Y")
 
@@ -160,7 +168,7 @@ def plot_pred(pressure):
     theta = jnp.linspace(0, 2 * jnp.pi, 200)
     x = piston_radius * jnp.cos(theta)
     y = piston_radius * jnp.sin(theta)
-    ax.plot(x, y, "w--", linewidth=2)  # 'w--' = white dashed line
+    ax.plot(x, y, "k--", linewidth=2)  # 'w--' = white dashed line
 
     ax.set_aspect("equal", "box")
     return fig
@@ -169,12 +177,14 @@ def plot_pred(pressure):
 # These are infinitely iterable
 infinite_data_loader = iter(data_generator)
 infinite_point_loader = iter(domain_generator)
-infinite_disk_loader = iter(source_plane_generator)
+infinite_disk_loader = iter(mesh_generator)
 
 # Initialize logger
 logger = TensorboardLogger(experiment_name=f"test-run_{len(data_generator)}")
 logger.log_plot("plots/p_gt_mag", plot_pred, jnp.abs(eval_pressure_pc.vals), 0)
 logger.log_plot("plots/p_gt_phase", plot_pred, jnp.angle(eval_pressure_pc.vals), 0)
+logger.log_plot("plots/v_gt_mag", plot_pred, jnp.abs(eval_velocity_pc.vals), 0)
+logger.log_plot("plots/v_gt_phase", plot_pred, jnp.angle(eval_pressure_pc.vals), 0)
 log_every = 1000
 
 # Random Fourier features for input coordinates helps with low-frequency bias
@@ -198,11 +208,22 @@ pinn = HelmholtzPINN.create(
     key=net_key,
 )
 
+
 # v_n is a circular 'step function'
-velocity_model = (
-    lambda params, *x: params if jnp.array(x).sum() ** 2 <= piston_radius**2 else 0.0
-)
-velocity_params = jnp.array(piston_velocity)
+def velocity_model(params, *x):
+    alpha, velocity = params
+    # unpack coordinates
+    x = jnp.array(x)
+    r = jnp.linalg.norm(x[:2])  # radius in the xy-plane
+
+    # smooth indicator
+    inside_smooth = jax.nn.sigmoid(-(r - piston_radius) * alpha)
+
+    # velocity is params inside the radius, 0 outside, smooth transition
+    return inside_smooth * velocity
+
+
+velocity_params = (jnp.array(50.0), jnp.array(piston_velocity))
 
 # Extract the trainable parameters of the neural-net as a `PyTree`
 pinn_params, _ = eqx.partition(pinn.model, filter_spec=eqx.is_array)
@@ -210,11 +231,13 @@ pinn_params = jax.tree.map(
     lambda x: x / jnp.sqrt(2), pinn_params
 )  # scaling due to complex
 
+
 # Initialize optimizers
 learning_rate = optax.schedules.exponential_decay(1e-3, 2000, 0.9)
 fwd_optimizer = optax.contrib.split_real_and_imaginary(
     soap(learning_rate, precondition_frequency=2)
 )
+learning_rate = optax.schedules.exponential_decay(1e-2, 10_000, 0.9)
 inv_optimizer = optax.contrib.split_real_and_imaginary(optax.sgd(learning_rate))
 
 fwd_opt_state = fwd_optimizer.init(pinn_params)
@@ -259,12 +282,11 @@ def inv_train_step(model, params, inv_model, inv_params, opt_state, batch):
     grads_conj = jax.tree.map(jnp.conj, grads)
     updates, opt_state = inv_optimizer.update(grads_conj, opt_state, inv_params)
     inv_params = optax.apply_updates(inv_params, updates)
-
     return inv_params, opt_state, val
 
 
 # Training loop: all the optimization work is done here + logging
-total_steps = int(20e3) + 1
+total_steps = int(50e3) + 1
 for step, data_batch, pde_batch, bnd_batch in tqdm(
     zip(
         range(total_steps),
@@ -305,12 +327,28 @@ for step, data_batch, pde_batch, bnd_batch in tqdm(
             pinn_params, pinn, batch, losses, extra_args=extra_args
         )
         loss_weights = update_weights(0.9, loss_weights, new_weights)
-        # introduce weights scheduler
+
+    # Boundary condition masking
+    filtered_loss_terms = {k: v for k, v in individual_losses.items() if k != "bnd"}
+    filtered_loss_sum = sum(filtered_loss_terms.values())
+    if step == 0:
+        b = filtered_loss_sum  # save first step to calibrate masking fn
+    loss_weights["bnd"] *= compute_mask(filtered_loss_sum, 1.0, b, 1e-3)  # pyright: ignore
 
     if step % log_every == 0:
         logger.log_scalar("loss/total", loss, step)
         logger.log_scalar("inv/loss", inv_loss, step)
-        logger.log_scalar("inv/velocity-error", velocity_params - piston_velocity, step)  # pyright: ignore
+        logger.log_scalar(
+            "inv/percent-relative-velocity-error",
+            100 * (jnp.abs(velocity_params[1] - piston_velocity) / piston_velocity),
+            step,
+        )
+
+        logger.log_scalar(
+            "inv/v_pred-minus-v_gt",
+            velocity_params[1] - piston_velocity,
+            step,
+        )
 
         for term, weight in loss_weights.items():
             logger.log_scalar(f"weight/{term}", weight, step)
@@ -319,21 +357,46 @@ for step, data_batch, pde_batch, bnd_batch in tqdm(
             logger.log_scalar(f"loss/{term}", loss / loss_weights.get(term), step)
 
         # make a prediction
-        p_pred = compute_pressure(pinn_params, pinn)
-        v_pred = compute_velocity(pinn_params, pinn)
-        p_pred_error = split_real_and_imaginary_metric(aggregated_metrics["mse"])(
-            p_pred, eval_pressure_pc.vals
-        )
-
-        logger.log_scalar("errors/mse_re", p_pred_error.real, step)
-        logger.log_scalar("errors/mse_im", p_pred_error.imag, step)
+        p_pred = predict_pressure(pinn_params, pinn)
+        v_pred = predict_velocity(pinn_params, pinn)
 
         logger.log_plot("plots/p_pred_mag", plot_pred, jnp.abs(p_pred), step)
         logger.log_plot("plots/p_pred_phase", plot_pred, jnp.angle(p_pred), step)
         logger.log_plot("plots/v_pred_mag", plot_pred, jnp.abs(v_pred), step)
         logger.log_plot("plots/v_pred_phase", plot_pred, jnp.angle(v_pred), step)
 
+        # compute errors
+        p_pred_error = split_real_and_imaginary_metric(aggregated_metrics["mse"])(
+            p_pred, eval_pressure_pc.vals
+        )
+
+        v_pred_error = split_real_and_imaginary_metric(aggregated_metrics["mse"])(
+            v_pred, eval_pressure_pc.vals
+        )
+
+        logger.log_scalar("errors/p_mse_re", p_pred_error.real, step)
+        logger.log_scalar("errors/p_mse_im", p_pred_error.imag, step)
+        logger.log_scalar("errors/v_mse_re", v_pred_error.real, step)
+        logger.log_scalar("errors/v_mse_im", v_pred_error.imag, step)
+
         # compute point-wise metrics
-        error = split_real_and_imaginary_metric(sq_error)(p_pred, eval_pressure_pc.vals)
-        logger.log_plot("errors/sq_error_re", plot_pred, jnp.real(error), step)
-        logger.log_plot("errors/sq_error_im", plot_pred, jnp.imag(error), step)
+        p_error = split_real_and_imaginary_metric(sq_error)(
+            p_pred, eval_pressure_pc.vals
+        )
+        logger.log_plot("errors/p_sq_error_re", plot_pred, jnp.real(p_error), step)
+        logger.log_plot("errors/p_sq_error_im", plot_pred, jnp.imag(p_error), step)
+
+        v_error = split_real_and_imaginary_metric(sq_error)(
+            v_pred, eval_velocity_pc.vals
+        )
+        logger.log_plot("errors/v_sq_error_re", plot_pred, jnp.real(v_error), step)
+        logger.log_plot("errors/v_sq_error_im", plot_pred, jnp.imag(v_error), step)
+
+        # what the 'velcity model' looks like
+        v_inv_pred = jax.vmap(velocity_model, [None, 0, 0, 0])(
+            velocity_params, *eval_velocity_pc.coords
+        )
+        logger.log_plot("plots/v_inv_pred_mag", plot_pred, jnp.abs(v_inv_pred), step)
+        logger.log_plot(
+            "plots/v_inv_pred_phase", plot_pred, jnp.angle(v_inv_pred), step
+        )
